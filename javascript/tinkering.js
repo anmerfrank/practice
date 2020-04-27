1b987f55480ce86f9ba3a8a4799aa5493b7ad6b3
Anne Franklin <anmerfrank@gmail.com>
2020-04-26 18:25:54 -0700

    more tinkering

--- javascript/tinkering.js
+++ javascript/tinkering.js
@@ -1,11 +1,22 @@
-const raining = true;
-const cold = false; 
+// const raining = true;
+// const cold = false; 
 
-if (raining) {
-  console.log("Don't forget your umbrella!")
-}
-if (cold) {
-  console.log("Make sure you pick out a scarf!")
+// if (raining) {
+//   console.log("Don't forget your umbrella!")
+// }
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!")
+// }
+
+// console.log("Now you're ready to go out!")
+
+
+function concat(array1, array2) {
+  return array1.concat(array2);
 }
 
-console.log("Now you're ready to go out!")
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
